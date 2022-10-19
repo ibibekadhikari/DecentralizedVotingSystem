aec8ad0cf705a96f71685519bef34d61b64922e3
Bibek <ibibekadhikari@gmail.com>
2022-10-19 12:49:55 -0400

    Updated party registration UI v0.0.2

--- web3frontend/src/Components/PostParty/PostParty.js
+++ web3frontend/src/Components/PostParty/PostParty.js
@@ -17,7 +17,8 @@ useEffect(()=> {
 
 const [PartyData, setPartyData] = useState({
     p_name: '',
-    c_count: null
+    c_count: null,
+    e_name: ''
 })
 
 const postData = () => {
@@ -33,22 +34,36 @@ const handleChange = (e) => {
     e.preventDefault();
     PartyData[e.target.name] = e.target.value;
     PartyData["c_count"] = candidateCount.length;
+    PartyData["e_name"] = electionName;
     const newData = {...PartyData};
     setPartyData(newData);
 
 }
+
+const [isActive, setisActive] = useState(false);
+
+const [electionName, setElectionname] = useState('');
+
+const updateElectionName = (e) => {
+     const eName = e.target.value;
+        setElectionname(eName)
+        setisActive(true);
+}
+
+
+
 
   return (
     <div>
     <section className="vh-100 bg-image"
     style={{backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')"}}>
     <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-      <div className="container h-100" style={{marginTop: "-70px"}}>
+      <div className="container h-100" >
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-7 col-xl-6 ">
             <div className="card" style={{borderRadius: "15px"}}>
               <div className="card-body p-5 ">
-                <h2 className="text-center mb-5">Register Party Data.</h2>  
+                <h2 className="text-center mb-5">Register Party Data {electionName}.</h2>  
                 
                 <form>  
                   <div className="form-outline mb-2" >
@@ -71,7 +86,7 @@ const handleChange = (e) => {
           </div>
         </div>
       </div>
-      <div className="container h-100" style={{marginTop: "200px"}}>
+      <div className="container h-100" style={{marginTop: "260px"}}>
       <div className="col-12 col-md-8 col-lg-7 col-xl-6 ">
             <div className="card" style={{borderRadius: "15px"}}>
             <div className="card-body p-5 ">
@@ -81,7 +96,7 @@ const handleChange = (e) => {
         candidateCount.map((elements)=>{
           return (
             <>
-            <button className='btn btn-warning' style={{marginTop: "-5px"}}>{elements.e_name}</button>
+            <button className='btn btn-warning' style={{marginTop: "-5px", backgroundColor: isActive && electionName === elements.e_name? "#f0a046": "", }} value={elements.e_name} onClick={(e) => updateElectionName(e)}>{elements.e_name}</button>
 
             </>
           )
